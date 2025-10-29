24f3d801041ac09cdb14e3c45f12379f412d23a0
Leblanc <ogaleblanc@gmail.com>
2025-10-29 13:09:42 +0100

    New Features added to game

--- lib/games/tic_tac_toe.dart
+++ lib/games/tic_tac_toe.dart
@@ -45,11 +45,83 @@ class _TicTacToeState extends State<TicTacToe> {
   //on box tap logic
   void _tapped(int index) {
     setState(() {
-      displayExOh[index] = 'o';
+      if (ohTurn) {
+        displayExOh[index] = 'o';
+      } else {
+        displayExOh[index] = 'x';
+      }
+
+      ohTurn = !ohTurn;
+      _checkWinner();
     });
   }
 
-  void _checkWinner9() {}
+  // This is where we check for the winner
+  void _checkWinner() {
+    //checks 1st row
+    if (displayExOh[0] == displayExOh[1] &&
+        displayExOh[0] == displayExOh[2] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
 
-  void _showWinDialog() {}
+    //checks 2nd row
+    if (displayExOh[3] == displayExOh[4] &&
+        displayExOh[3] == displayExOh[5] &&
+        displayExOh[3] != '') {
+      _showWinDialog(displayExOh[3]);
+    }
+
+    //checks 3rd row
+    if (displayExOh[6] == displayExOh[7] &&
+        displayExOh[6] == displayExOh[8] &&
+        displayExOh[6] != '') {
+      _showWinDialog(displayExOh[6]);
+    }
+
+    //checks 1st column
+    if (displayExOh[0] == displayExOh[3] &&
+        displayExOh[0] == displayExOh[6] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    //checks 2nd column
+    if (displayExOh[1] == displayExOh[4] &&
+        displayExOh[1] == displayExOh[7] &&
+        displayExOh[1] != '') {
+      _showWinDialog(displayExOh[1]);
+    }
+
+    //checks 3rd column
+    if (displayExOh[2] == displayExOh[5] &&
+        displayExOh[2] == displayExOh[8] &&
+        displayExOh[2] != '') {
+      _showWinDialog(displayExOh[2]);
+    }
+
+    //checks right diagonal
+    if (displayExOh[0] == displayExOh[4] &&
+        displayExOh[0] == displayExOh[8] &&
+        displayExOh[0] != '') {
+      _showWinDialog(displayExOh[0]);
+    }
+
+    // checks left diagonal
+    if (displayExOh[6] == displayExOh[4] &&
+        displayExOh[6] == displayExOh[2] &&
+        displayExOh[6] != '') {
+      _showWinDialog(displayExOh[6]);
+    }
+  }
+
+  // Dialog Box to show winner
+  void _showWinDialog(String winner) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(title: Text("Player $winner Wins!"));
+      },
+    );
+  }
 }
